f9ea9b84b4db3520e10c2e7779788a1fcaf118fd
andronikmk <andronikmk@gmail.com>
2020-07-21 19:11:07 -0400

    Switch to telegram contact me

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -13,11 +13,11 @@ const Author = () => {
         {author.bio}
         <a
           className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
+          href={getContactHref('telegram', author.contacts.telegram)}
           rel="noopener noreferrer"
           target="_blank"
         >
-          <strong>{author.name}</strong> on Twitter
+          <strong>{author.name}</strong> on Telegram
         </a>
       </p>
     </div>
